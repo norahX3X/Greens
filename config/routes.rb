95ec69c610080205787c23c6aea8601378652a33
norahx3x <alnoor0416@gmail.com>
2019-04-22 12:10:41 +0300

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'carts/index'
   resources :categories
   resources :products
   devise_for :users
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,11 @@ ProductInfo.create(product_id: apple.id, price: 5.0,stack: 5 , seller_id:1)
 
 vigi = Category.create(name: 'vigi')
 lettuce = Product.create(name: 'lettuce', image: 'https://banner2.kisspng.com/20180328/qwe/kisspng-lettuce-sandwich-butterhead-lettuce-vegetable-sala-lettuce-5abb79e2ca0f80.4373410515222358748277.jpg', quantity: 15, category_id: vigi.id)
-# order =Order.create(user_id: 1, total: 23.5 , quantity: 4 , shippingID: 1)
-# OrderDetail.create(user_id: order.id , product_id: 1, price:12.5 , amount: 1 )
-# OrderDetail.create(user_id: order.id , product_id: 2, price:0.5 , amount: 1 )
-# OrderDetail.create(user_id: order.id , product_id: 3, price:30.5 , amount: 1 )
-# OrderDetail.create(user_id: order.id , product_id: 4, price:4.5 , amount: 1 )
+
+
+shippingAdd = ShippingInfo.create(user_id: 1, zip: 11543, adress:" najah al izz - Al Olaya " ,city: "Riyadh", phone_num: "0550585468" )
+order =Order.create(user_id: 1, total: 23.5 , quantity: 4 , shipping_id: shippingAdd.id )
+OrderDetail.create(order_id: order.id , product_id: 1, price:12.5 , amount: 1 )
+OrderDetail.create(order_id: order.id , product_id: 2, price:0.5 , amount: 1 )
+OrderDetail.create(order_id: order.id , product_id: 3, price:30.5 , amount: 1 )
+OrderDetail.create(order_id: order.id , product_id: 4, price:4.5 , amount: 1 )
