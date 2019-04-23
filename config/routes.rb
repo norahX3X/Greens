8266f0518306bd6673db15821881a69eb0e44301
norahx3x <alnoor0416@gmail.com>
2019-04-23 13:31:04 +0300

    add to cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  post 'products/addCart'
+
   resources :orders
+  # post '/add_to_cart/:product_id' => 'carts#add_to_cart', :as => 'add_to_cart'
+  # post 'addCart' => 'product#addCart'
+
+  # get '/addCart', to: 'ProductsController#addCart', as: 'addCart'
+
 #   root 'orders#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
