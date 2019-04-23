2c40423feab6e189afbc15cd16ec26701ae6e30c
norahx3x <alnoor0416@gmail.com>
2019-04-23 11:10:11 +0300

    counter

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'about' => 'pages#about'
+  get '/profile' => 'pages#profile'
 end
