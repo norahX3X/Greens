d28c8f202e30ae2204dae5d28766c554cd5d0e71
norahx3x <alnoor0416@gmail.com>
2019-04-24 15:43:53 +0300

    heroku

--- db/migrate/20190421142520_remove_columns_from_product_info.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromProductInfo < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :product_infos, :productID , :integer
-    remove_column :product_infos, :sellerID , :integer
-    add_column :product_infos, :product_id , :integer
-    add_column :product_infos, :seller_id , :integer
-  end
-end
